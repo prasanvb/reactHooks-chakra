40bc5a2baa4999a28c58b9c0a27b634e96355d28
prasanv <prasan.venkatb@gmail.com>
2022-01-09 18:18:02 -0800

    apply formating the usestate hook examples

--- src/Components/usestate/1.UsestateHook.js
+++ src/Components/usestate/1.UsestateHook.js
@@ -1,36 +1,46 @@
-import { Button, Text, Container } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import { Button, Text, Container } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 const UsestateHook = () => {
   const initialCount = 0;
   const [counter, setCounter] = useState(initialCount);
 
   const onIncrement = () => {
-    setCounter((oldValue) => oldValue+1);
-  }
-  
+    setCounter((oldValue) => oldValue + 1);
+  };
+
   const onDecrement = () => {
-    setCounter((oldValue) => oldValue-1);
-  }
-  
+    setCounter((oldValue) => oldValue - 1);
+  };
+
   const onReset = () => {
     setCounter(initialCount);
-  }
-  
+  };
+
   const onIncrementBy5 = () => {
-    setCounter((oldValue) => oldValue+5);
-  }
+    setCounter((oldValue) => oldValue + 5);
+  };
 
   return (
     <Container m={5} p={5}>
-      <Text fontSize={20} textDecoration="underline">useState Hook Example 1</Text>
+      <Text fontSize={20} textDecoration="underline">
+        useState Hook Example 1
+      </Text>
       <p>Counter = {counter}</p>
-      <Button onClick={onIncrement} m={2}>Increment</Button> 
-      <Button onClick={onDecrement} m={2}>Decrement</Button> 
-      <Button onClick={onReset} m={2}>Reset</Button> 
-      <Button onClick={onIncrementBy5} m={2}>IncrementBy5</Button> 
+      <Button onClick={onIncrement} m={2}>
+        Increment
+      </Button>
+      <Button onClick={onDecrement} m={2}>
+        Decrement
+      </Button>
+      <Button onClick={onReset} m={2}>
+        Reset
+      </Button>
+      <Button onClick={onIncrementBy5} m={2}>
+        IncrementBy5
+      </Button>
     </Container>
-  )
-}
+  );
+};
 
 export default UsestateHook;
--- src/Components/usestate/2.UsestateObject.js
+++ src/Components/usestate/2.UsestateObject.js
@@ -1,27 +1,43 @@
-import { Input, Text, Container } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import { Input, Text, Container } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 const UsestateObject = () => {
   const detailsObj = {
-    firstname: '',
-    lastname: ''
-  }
+    firstname: "",
+    lastname: "",
+  };
   const [userDetails, setUserDetails] = useState(detailsObj);
 
   const firstnameOnChange = (e) => {
-    setUserDetails((old)=>{ return {...old, firstname: e.target.value}})
-  }
+    setUserDetails((old) => {
+      return { ...old, firstname: e.target.value };
+    });
+  };
 
   return (
     <Container m={5} p={5}>
-      <Text fontSize={20} textDecoration="underline">useState Object Example 2</Text>
+      <Text fontSize={20} textDecoration="underline">
+        useState Object Example 2
+      </Text>
       <form>
-        <Input type="text" value={userDetails.firstname} onChange={(e)=>{firstnameOnChange(e)}}></Input>
-        <Input type="text" value={userDetails.lastname} onChange={(e)=>setUserDetails({...userDetails, lastname: e.target.value})}></Input>
+        <Input
+          type="text"
+          value={userDetails.firstname}
+          onChange={(e) => {
+            firstnameOnChange(e);
+          }}
+        ></Input>
+        <Input
+          type="text"
+          value={userDetails.lastname}
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, lastname: e.target.value })
+          }
+        ></Input>
         {JSON.stringify(userDetails)}
       </form>
     </Container>
-  )
-}
+  );
+};
 
-export default UsestateObject
+export default UsestateObject;
