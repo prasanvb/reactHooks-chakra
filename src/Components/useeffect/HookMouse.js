b2920af977f4669cf19598062fddc94573746017
prasanv <prasan.venkatb@gmail.com>
2022-08-16 16:04:17 -0700

    minor code cleanup

--- src/Components/useeffect/HookMouse.js
+++ src/Components/useeffect/HookMouse.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Button, Text, Container } from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
+
 
 const HookMouse = () => {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [display, setDisplay] = useState(true);
 
   const logPosition = (e) => {
-    // Note: If you dont cleaned up mouse logPosition is always run on every mouse move
+    // Note: If you don't cleaned up mouse logPosition is always run on every mouse move
     console.log("Mouse Event will always run as you move the mouse");
     setX(e.clientX);
     setY(e.clientY);
@@ -30,7 +30,7 @@ const HookMouse = () => {
 
   return (
     <Container m={5} p={5}>
-      <p>{display && `X - ${x}, Y - ${y}`}</p>
+      <p>{`X - ${x}, Y - ${y}`}</p>
     </Container>
   );
 };
