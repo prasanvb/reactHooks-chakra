6932c9c951c9cfe9a2343e6c47e1c0636a382af6
prasanvb <34255378+prasanvb@users.noreply.github.com>
2024-06-24 21:41:45 -0700

    minor to useContext compA (#1)

--- src/Components/usecontext/ComponentA.js
+++ src/Components/usecontext/ComponentA.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { userAuthDetails } from "../../../pages/usecontext/index";
 import { Container } from "@chakra-ui/layout";
 import ComponentB from "../../Components/usecontext/ComponentB";
 
 const ComponentA = () => {
+  const [authDetails] = useContext(userAuthDetails);
   return (
     <Container p={5} m={5} border="1px solid" borderColor="red">
       ComponentA
+      <div>{`Logged in status: ${authDetails?.isLoggedIn}`}</div>
       <ComponentB></ComponentB>
     </Container>
   );
